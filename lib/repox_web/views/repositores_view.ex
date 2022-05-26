e2d0b697b4df90040eac2117ae6f2bb9f918eea8
Douglas Silva <doug.neo@gmail.com>
2022-05-26 01:34:08 -0300

    creating the view that will deliver the data to the application's api

---
+++ lib/repox_web/views/repositores_view.ex
@@ -0,0 +1,17 @@
+defmodule RepoxWeb.RepositoriesView do
+  use RepoxWeb, :view
+
+  def render("index.json", %{repositories: repositories}) do
+    %{
+      repositories: Enum.map(repositories, fn repository ->
+          %{
+            name: repository["name"],
+            description: repository["description"],
+            id: repository["id"],
+            html_url: repository["html_url"],
+            stargazers_count: repository["stargazers_count"]
+          }
+        end)
+    }
+  end
+end
