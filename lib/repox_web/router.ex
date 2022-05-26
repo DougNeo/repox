ba0812c5cb280437515885ec07d20ece80650d38
Douglas Silva <doug.neo@gmail.com>
2022-05-26 01:28:26 -0300

    defining routes

--- lib/repox_web/router.ex
+++ lib/repox_web/router.ex
@@ -20,10 +20,11 @@ defmodule RepoxWeb.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", RepoxWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", RepoxWeb do
+    pipe_through :api
+
+    get "/repositories/:username", RepositoriesController, :index
+  end
 
   # Enables LiveDashboard only for development
   #
